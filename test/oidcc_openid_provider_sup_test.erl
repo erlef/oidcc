4ba98021ca70d549a2d0663f4bd26678092690e6
Bas Wegh <bas.wegh@kit.edu>
2016-04-18 15:25:27 +0100

    add tests for the openid_provider_sup

--- test/oidcc_openid_provider_sup_test.erl
+++ test/oidcc_openid_provider_sup_test.erl
@@ -1,13 +1,17 @@
 -module(oidcc_openid_provider_sup_test).
 -include_lib("eunit/include/eunit.hrl").
 
-
-get_list_test() ->
-    % can't be tested using unittests
-    % as the supervisor can't be mocked
-    ok.
-
-add_test() ->
-    % can't be tested using unittests
-    % as the supervisor can't be mocked
+overall_test() ->
+    {ok, _} = oidcc_openid_provider_sup:start_link(),
+    {ok, []} = oidcc_openid_provider_sup:get_openid_provider_list(),
+    {ok, _Id, _Pid} = oidcc_openid_provider_sup:add_openid_provider(),
+    Id = <<"MyCoolId">>,
+    {ok, Id, Pid} = oidcc_openid_provider_sup:add_openid_provider(Id),
+    {ok, Pid} = oidcc_openid_provider_sup:get_openid_provider(Id),
+    {error, not_found} = oidcc_openid_provider_sup:get_openid_provider(<<"123">>),
+    {error, id_already_used} = oidcc_openid_provider_sup:add_openid_provider(Id),
+    {ok, UndefinedId, _} = oidcc_openid_provider_sup:add_openid_provider(undefined),
+    false = (UndefinedId == undefined),
+    {ok, List} = oidcc_openid_provider_sup:get_openid_provider_list(),
+    3 = length(List),
     ok.

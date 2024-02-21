44cfca2b2c85df63192d32d0d929a3c676d3ce2c
Jonatan Männchen <jonatan@maennchen.ch>
2024-02-21 21:07:24 +0100

    Link to Conformance Repository

--- .formatter.exs
+++
@@ -1,5 +0,0 @@
-# Used by "mix format"
-[
-  import_deps: [:phoenix],
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
-]
--- config/config.exs
+++
@@ -1,7 +0,0 @@
-import Config
-
-config :conformance, Conformance.Endpoint,
-  http: [ip: {127, 0, 0, 1}, port: 4000],
-  server: true,
-  secret_key_base: String.duplicate("a", 64),
-  debug_errors: true

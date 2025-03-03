bb0e76f48240405f7a2b9d29c90ed50fd3a7b5d0
Jonatan Männchen <jonatan@maennchen.ch>
2025-03-03 09:47:24 +0000

    Handle Token Struct in Logout.initiate_url/3 (#427)

--- test/oidcc/logout_test.exs
+++ test/oidcc/logout_test.exs
@@ -2,11 +2,12 @@ defmodule Oidcc.LogoutTest do
   use ExUnit.Case, async: true
 
   alias Oidcc.Logout
+  alias Oidcc.Token
 
   doctest Logout
 
   describe inspect(&Logout.create_redirect_url/3) do
-    test "works" do
+    test "works with token string" do
       pid =
         start_supervised!(
           {Oidcc.ProviderConfiguration.Worker,
@@ -23,6 +24,38 @@ defmodule Oidcc.LogoutTest do
       assert {:ok, _redirect_uri} =
                Logout.initiate_url(
                  "token",
+                 client_context,
+                 %{post_logout_redirect_uri: "https://my.server/return"}
+               )
+    end
+
+    test "works with token struct" do
+      pid =
+        start_supervised!(
+          {Oidcc.ProviderConfiguration.Worker,
+           %{issuer: "https://erlef-test-w4a8z2.zitadel.cloud"}}
+        )
+
+      {:ok, client_context} =
+        Oidcc.ClientContext.from_configuration_worker(
+          pid,
+          "client_id",
+          :unauthenticated
+        )
+
+      token = %Token{
+        id: %Token.Id{
+          token: "token",
+          claims: %{}
+        },
+        access: %Token.Access{token: "access_token", expires: :undefined},
+        refresh: %Token.Refresh{token: "refresh_token"},
+        scope: ["profile", "openid"]
+      }
+
+      assert {:ok, _redirect_uri} =
+               Logout.initiate_url(
+                 token,
                  client_context,
                  %{post_logout_redirect_uri: "https://my.server/return"}
                )
